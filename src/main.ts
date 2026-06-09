92a2b0417c5bae936ab1a561a8016bf66ca664af
omnakum2 <omnakum2@gmail.com>
2026-06-09 15:24:10 +0530

    change the ip setting to prefer ipv4

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './AppModule';
 import { UserService } from './Modules/User/UserService';
 import { createSuperAdmin } from './Modules/User/superAdminSeed';
+import * as dns from 'node:dns';
+
+dns.setDefaultResultOrder('ipv4first');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
