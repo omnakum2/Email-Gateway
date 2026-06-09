096cbb4a44741165018135af88c0e81f10d3438c
omnakum2 <omnakum2@gmail.com>
2026-06-09 16:01:33 +0530

    added connection endpoint

--- src/Modules/Auth/AuthController.ts
+++ src/Modules/Auth/AuthController.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Param, Post, Request, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Request, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AuthLoginReponseDto, AuthLoginRequestDto } from './Dtos/AuthLoginRouteDto';
 import { AuthUpdatePasswordDTO } from './Dtos/AuthUpdatePasswordDto';
 import { AuthService } from './AuthService';
 import { AuthGuard } from './AuthGuard';
 import type { IUserRequest } from './AuthGuard';
+import * as net from 'net';
 
 @ApiTags('Users')
 @Controller('auth')
@@ -17,8 +18,28 @@ export class AuthController {
     return this.authService.logIn(userData.email, userData.password);
   }
 
+
+  @Get('smtp-test')
+  test() {
+    return new Promise((resolve, reject) => {
+      const socket = net.connect(587, 'smtp.gmail.com');
+
+      socket.on('connect', () => {
+        resolve('connected');
+        socket.destroy();
+      });
+
+      socket.on('error', reject);
+
+      socket.setTimeout(10000, () => {
+        socket.destroy();
+        reject(new Error('timeout'));
+      });
+    });
+  }
+
   @Post('update-password/:userId')
-  @UseGuards(AuthGuard) 
+  @UseGuards(AuthGuard)
   @ApiBearerAuth()
   @ApiResponse({ status: 201 })
   updatePassword(
