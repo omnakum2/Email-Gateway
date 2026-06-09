422a75ba8d233185f27b8de766c68b9a637417eb
omnakum2 <omnakum2@gmail.com>
2026-06-09 13:39:46 +0530

    change smtp config as per render environment

--- src/Providers/Nodemailer/NodemailerProvider.ts
+++ src/Providers/Nodemailer/NodemailerProvider.ts
@@ -9,7 +9,10 @@ export class NodemailerProvider implements MailProvider {
 
   constructor() {
     this.transporter = nodemailer.createTransport({
-      service: 'gmail',
+      host: process.env.EMAIL_HOST,
+      port: Number(process.env.EMAIL_PORT),
+      secure: false,
+      requireTLS: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
